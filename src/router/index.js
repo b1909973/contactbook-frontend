ef858ee76ad5a4d04d0ac7affed2f2236a7a7a23
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-08-20 12:36:47 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,18 @@ const routes = [
 path: "/",
 name: "contactbook",
 component: ContactBook,
-},{
-    path: "/:pathMatch(.*)*",
-    name: "notfound",
-    component: () => import("@/views/NotFound.vue"),
-    },
+},
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            name: "notfound",
+            component: () => import("@/views/NotFound.vue"),
+            },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
